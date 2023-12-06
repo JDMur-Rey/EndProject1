691550c4d78e4810d4d372116752e4c7f6905ae3
Jack <jackdreynolds@outlook.com>
2023-12-05 21:24:10 -0700

    Updating Mysql code and JS in meeting-creation

--- EP1/src/client.sql
+++ EP1/src/client.sql
@@ -1,6 +1,6 @@
-create table client {
+create table client (
     id int primary key auto_increment,
     first_name varchar(250),
     last_name varchar(250),
     email varchar(500)
-}
+)
--- EP1/src/meeting.sql
+++ EP1/src/meeting.sql
@@ -1,10 +1,10 @@
-create table client {
-    client_id int primary key auto_increment,
+create table meeting (
+    client_id int auto_increment,
     first_name varchar(250),
-    last_name varchar(250),
     email varchar(500),
     meeting_topic varchar(1024),
     date date,
     time time,
-    location varchar(1024)
-}
+    location varchar(1024),
+    FOREIGN KEY (client_id) REFERENCES client(id)
+)
